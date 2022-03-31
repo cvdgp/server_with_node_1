38a4c9af3cff5395c577e0831aaabf225e4ef64f
cvdgp <dagipa@hotmail.es>
2022-03-31 22:17:05 +0200

    cargados archivos node

---
+++ assents/js/index.js
@@ -0,0 +1,18 @@
+const math = require("./main.js");
+/* Modulo http para conexion a servidor*/
+const http = require(`http`)
+const os = require("os");
+
+console.log(os.platform());
+
+
+/*crear un servidor */
+
+http.createServer(
+    function(req, res){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(`<p>Texto cargadao con node js</p>`)
+        res.end();
+
+    }
+).listen(3000);
---
+++ assents/js/main.js
@@ -0,0 +1,16 @@
+
+const math = {};
+
+const  sumar        =       (a,b)   => a+b;
+const  restar       =       (a,b)   => a-b;
+const  dividir      =       (a,b)   => a/b;
+const  multiplicar  =       (a,b)   => a*b;
+
+
+
+math.sumar = sumar; 
+math.restar = restar; 
+math.dividir = dividir; 
+math.multiplicar = multiplicar; 
+
+module.export = math;
